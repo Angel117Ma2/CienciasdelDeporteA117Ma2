17e0cae3564ae9e2f8761fc8c01fc6b3f251404c
Angel117Ma2 <LeonKennedy4Scoot@outlook.com>
2024-06-21 14:25:07 -0600

    Add files via upload

---
+++ me gusta.php
@@ -0,0 +1,333 @@
+<!Doctype html> 
+<html>
+              <head> 
+                    <title>Ciencias del Deporte A117</title>
+                    <link rel="stylesheet" href="css/estilo.css">
+                    <link rel="shortcut icon" type="image/x-ico" href="imagenes/favicon.png">
+              </head>
+             
+<body>
+
+  
+
+     <header>
+            <h2>Ciencias del Deporte A117</h2>
+                                             <img src="imagenes/favicon.png" width="300px"  class="logo">
+                                             <input type="checkbox" id="check">
+                                             <label for="check" class="mostrar-menu">
+                                                &#8801
+                                             </label>
+    
+     <nav class="menu">
+           <a href="https://tejidomuscular.netlify.app/">Tejido muscular</a>
+           <a href="https://fibrasmusculares.netlify.app/">Fibras musculares</a>
+           <a href="https://sistemas-de-energia.netlify.app/">Sistemas de energia</a>
+           <a href="https://playful-sundae-27efc8.netlify.app/">Ejercicios en casa</a>
+           <a href="https://blueteam1.netlify.app/">Solo amigos</a>
+           <a href="https://markvproyect.netlify.app/">Proyecto MarkV</a>
+           <label for="check" class="esconder-menu">
+            &#215
+           </label>
+     </nav>
+ </header>
+
+<section id="persona">
+                    <div class="s">
+                        <img src="imagenes/Creatina.png" width="300px">
+                      <h2>La creatina</h2>
+                          <p>
+                            te ayudara a mejorar tu rendimiento deportivo, el uso oral de la creatina,
+                            puede permitirle a un atleta trabajar mas durante repeticiones o carreras cortas, lo que lo hace ganar mas fuerza y tamaño muscular. Nuestro cuerpo ya produce de forma natural este 
+                            aminoacido, mas sin embargo las cantidades que se almacenan en nuestras celulas musculares son muy limitadas.
+                          </p>
+                          <a href="https://creatina.netlify.app/" class="boton">MAS INFORMACION</a>
+                        </div>
+                        
+                        <div class="a">
+                            <img src="imagenes/latigazo.png" width="300px">
+                            <h2>El latigazo cervical</h2>
+                            <p>
+                                Se trata de una patologia que se produce por una brusca flexion y extension del cuello.
+                            </p>
+                            <a href="https://latigazo-cervical.netlify.app/" class="boton">MAS INFORMACION</a>
+                        </div>
+
+                        <div class="b">
+                            <img src="imagenes/proteina.png" width="300px">
+                            <h2>La proteina</h2>
+                            <p>
+                                Consumir proteina ayudara a tu cuerpo a reparar celulas ademas de producir nuevas. Las proteinas jugan un papel importante
+								para la mayoria de funciones biologicas que el cuerpo realiza, el crecimiento y el desarrollo de niños es uno de ellos, ademas
+								de la produccion de enzimas y hormonas y la funcion inmunologica. 
+                            </p>
+                            <a href="https://proteina.netlify.app/" class="boton">MAS INFORMACION</a>
+                        </div>
+
+                        <div class="c">
+                            <img src="imagenes/torax.png">
+                            <h2>El sindrome del desfiladero toracico</h2>
+                            <p>
+                                Es una patologia que tiene lugar por la compresion de las estructuras neurovasculares.
+                            </p>
+                            <a href="https://desfiladero-toracico.netlify.app/" class="boton">MAS INFORMACION</a>
+                        </div>
+</section>
+
+<a href="https://www.iubenda.com/condiciones-de-uso/25413883" class="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe " title="Términos y Condiciones ">Términos y Condiciones</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+<a href="https://www.iubenda.com/privacy-policy/25413883/cookie-policy" class="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe " title="Política de Cookies ">Política de Cookies</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+<a href="https://www.iubenda.com/privacy-policy/25413883" class="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe " title="Política de Privacidad ">Política de Privacidad</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+    <link href="css/estilos.css" rel="stylesheet" type="text/css" />
+    <script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
+
+<section class="Lesi">
+    <nav class="navbar navbar-default">
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="./">INICIO <span class="sr-only">(current)</span></a></li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="panel-body">
+
+                    <!--Inicio elementos contenedor-->
+
+                    <div class="comment-form-container">
+                        <form id="frm-comment">
+                            <div class="input-row">
+                                <input type="hidden" name="comentario_id" id="commentId" placeholder="Name" /> <input class="input-field" type="text" name="name" id="name" placeholder="Agrega un nombre" />
+                            </div>
+                            <div class="input-row">
+                                <textarea class="input-field" type="text" name="comment" id="comment" placeholder="Agregar comentario"> </textarea>
+                            </div>
+                            <div>
+                                <input type="button" class="btn-submit" id="submitButton" value="Publicar Ahora" />
+                                
+                            </div>
+                            <div style="clear:both"></div>
+                        </form>
+                    </div>
+                    <div id="output"></div>
+</section>
+                    <script>
+                        var totalLikes = 0;
+                        var totalUnlikes = 0;
+
+                        function postReply(commentId) {
+                            $('#commentId').val(commentId);
+                            $("#name").focus();
+                        }
+
+                        $("#submitButton").click(function() {
+                            $("#comment-message").css('display', 'none');
+                            var str = $("#frm-comment").serialize();
+
+                            $.ajax({
+                                url: "AgregarComentario.php",
+                                data: str,
+                                type: 'post',
+                                success: function(response) {
+                                    var result = eval('(' + response + ')');
+                                    if (response) {
+                                        $("#comment-message").css('display', 'inline-block');
+                                        $("#name").val("");
+                                        $("#comment").val("");
+                                        $("#commentId").val("");
+                                        listComment();
+                                    } else {
+                                        alert("Failed to add comments !");
+                                        return false;
+                                    }
+                                }
+                            });
+                        });
+
+                        $(document).ready(function() {
+                            listComment();
+                        });
+
+                        function listComment() {
+                            $.post("ListaDeComentarios.php",
+                                function(data) {
+                                    var data = JSON.parse(data);
+
+                                    var comments = "";
+                                    var replies = "";
+                                    var item = "";
+                                    var parent = -1;
+                                    var results = new Array();
+
+                                    var list = $("<ul class='outer-comment'>");
+                                    var item = $("<li>").html(comments);
+
+                                    for (var i = 0;
+                                        (i < data.length); i++) {
+                                        var commentId = data[i]['comentario_id'];
+                                        parent = data[i]['parent_comentario_id'];
+
+                                        var obj = getLikesUnlikes(commentId);
+
+                                        if (parent == "0") {
+                                            if (data[i]['like_unlike'] >= 1) {
+                                                like_icon = "<img src='img/MeGusta.png'  id='unlike_" + data[i]['comentario_id'] + "' class='like-unlike'  onClick='likeOrDislike(" + data[i]['comentario_id'] + ",-1)' />";
+                                                like_icon += "<img style='display:none;' src='img/NoMeGusta.png' id='like_" + data[i]['comentario_id'] + "' class='like-unlike' onClick='likeOrDislike(" + data[i]['comentario_id'] + ",1)' />";
+                                            } else {
+                                                like_icon = "<img style='display:none;' src='img/MeGusta.png'  id='unlike_" + data[i]['comentario_id'] + "' class='like-unlike'  onClick='likeOrDislike(" + data[i]['comentario_id'] + ",-1)' />";
+                                                like_icon += "<img src='img/NoMeGusta.png' id='like_" + data[i]['comentario_id'] + "' class='like-unlike' onClick='likeOrDislike(" + data[i]['comentario_id'] + ",1)' />";
+
+                                            }
+
+                                            comments = "\
+                                        <div class='comment-row'>\
+                                            <div class='comment-info'>\
+                                                <span class='commet-row-label'>De</span>\
+                                                <span class='posted-by'>" + data[i]['comment_sender_name'] + "</span>\
+                                                <span class='commet-row-label'>a las </span> \
+                                                <span class='posted-at'>" + data[i]['date'] + "</span>\
+                                            </div>\
+                                            <div class='comment-text'>" + data[i]['comment'] + "</div>\
+                                            <div>\
+                                                <a class='btn-reply' onClick='postReply(" + commentId + ")'>Responder</a>\
+                                            </div>\
+                                            <div class='post-action'>\ " + like_icon + "&nbsp;\
+                                                <span id='likes_" + commentId + "'> " + totalLikes + " Me Gusta </span>\
+                                            </div>\
+                                        </div>";
+
+                                            var item = $("<li>").html(comments);
+                                            list.append(item);
+                                            var reply_list = $('<ul>');
+                                            item.append(reply_list);
+                                            listReplies(commentId, data, reply_list);
+                                        }
+                                    }
+                                    $("#output").html(list);
+                                });
+                        }
+
+                        function listReplies(commentId, data, list) {
+
+                            for (var i = 0;
+                                (i < data.length); i++) {
+
+                                var obj = getLikesUnlikes(data[i].comentario_id);
+                                if (commentId == data[i].parent_comentario_id) {
+                                    if (data[i]['like_unlike'] >= 1) {
+                                        like_icon = "<img src='img/MeGusta.png'  id='unlike_" + data[i]['comentario_id'] + "' class='like-unlike'  onClick='likeOrDislike(" + data[i]['comentario_id'] + ",-1)' />";
+                                        like_icon += "<img style='display:none;' src='img/NoMeGusta.png' id='like_" + data[i]['comentario_id'] + "' class='like-unlike' onClick='likeOrDislike(" + data[i]['comentario_id'] + ",1)' />";
+
+                                    } else {
+                                        like_icon = "<img style='display:none;' src='img/NoMeGusta.png'  id='unlike_" + data[i]['comentario_id'] + "' class='like-unlike'  onClick='likeOrDislike(" + data[i]['comentario_id'] + ",-1)' />";
+                                        like_icon += "<img src='img/NoMeGusta.png' id='like_" + data[i]['comentario_id'] + "' class='like-unlike' onClick='likeOrDislike(" + data[i]['comentario_id'] + ",1)' />";
+
+                                    }
+                                    var comments = "\
+                                        <div class='comment-row'>\
+                                            <div class='comment-info'>\
+                                                <span class='commet-row-label'>De </span>\
+                                                <span class='posted-by'>" + data[i]['comment_sender_name'] + "</span>\
+                                                <span class='commet-row-label'>a las </span> \
+                                                <span class='posted-at'>" + data[i]['date'] + "</span>\
+                                            </div>\
+                                            <div class='comment-text'>" + data[i]['comment'] + "</div>\
+                                            <div>\
+                                                <a class='btn-reply' onClick='postReply(" + data[i]['comentario_id'] + ")'>Responder</a>\
+                                            </div>\
+                                            <div class='post-action'> " + like_icon + "&nbsp;\
+                                                <span id='likes_" + data[i]['comentario_id'] + "'> " + totalLikes + " Me Gusta </span>\
+                                            </div>\
+                                        </div>";
+
+                                    var item = $("<li>").html(comments);
+                                    var reply_list = $('<ul>');
+                                    list.append(item);
+                                    item.append(reply_list);
+                                    listReplies(data[i].comentario_id, data, reply_list);
+                                }
+                            }
+                        }
+
+                        function getLikesUnlikes(commentId) {
+
+                            $.ajax({
+                                type: 'POST',
+                                async: false,
+                                url: 'Envio_MeGusta.php',
+                                data: {
+                                    comentario_id: commentId
+                                },
+                                success: function(data) {
+                                    totalLikes = data;
+                                }
+
+                            });
+
+                        }
+
+
+                        function likeOrDislike(comentario_id, like_unlike) {
+
+                            $.ajax({
+                                url: 'MeGusta_NoMeGusta.php',
+                                async: false,
+                                type: 'post',
+                                data: {
+                                    comentario_id: comentario_id,
+                                    like_unlike: like_unlike
+                                },
+                                dataType: 'json',
+                                success: function(data) {
+
+                                    $("#likes_" + comentario_id).text(data + " likes");
+
+                                    if (like_unlike == 1) {
+                                        $("#like_" + comentario_id).css("display", "none");
+                                        $("#unlike_" + comentario_id).show();
+                                    }
+
+                                    if (like_unlike == -1) {
+                                        $("#unlike_" + comentario_id).css("display", "none");
+                                        $("#like_" + comentario_id).show();
+                                    }
+
+                                },
+                                error: function(data) {
+                                    alert("error : " + JSON.stringify(data));
+                                }
+                            });
+                        }
+                    </script>
+
+                    <!--Fin elementos contenedor-->
+                </div>
+            </div>
+        </div>
+    </div>
+
+            
+                 
+</body>
+
+
+
+
+
+
+
+</html>
